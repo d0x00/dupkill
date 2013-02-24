ff2d38c4d02467b9981fd69da8f5b9c8467310f1
d0x00 <woot@mailbox.hu>
2013-02-24 10:24:58 +0100

    Update dupkill.py

--- dupkill.py
+++ dupkill.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 from urlparse import urlparse
 build = []
-outname = "" #output file here
-filename = "" #input file here
+outname = "" #output file
+filename = "" #input file
 def main():
 	cache()
 	for url in build:
@@ -10,30 +10,23 @@ def main():
 		for x in build:
 			if retlist(x) == info:
 				build.remove(x)
-				pass
-			pass
 		build.append(url)
-		pass
 	print build
 	i = 0
 	for x in build:
 		i += 1
-		pass
 	writeOut()
 	print "I got the job done, the remaining url's is {0}.".format(i)
-	pass
 def retlist(url):
 	if not "www." in urlparse(url).netloc:
 		url0= "www.{0}".format(urlparse(url).netloc)
 	else:
 		url0 = urlparse(url).netloc
-		pass
 	path = urlparse(url).path
 	url = urlparse(url)
 	a = []
 	for x in url.query.split("&"):
 		a.append(x.split("=")[0])
-		pass
 	ret = []
 	ret.append(url0)
 	ret.append(path)
@@ -44,14 +37,10 @@ def cache():
 	for sor in open(filename, "r"):
 		build.append(sor)
 		i += 1
-		pass
 	print "We start with {0} url's.".format(i)
-	pass
 def writeOut():
 	with open(outname, "w") as out:
 		for url in build:
 			out.write("{0}".format(url))
-			pass
-	pass
 if __name__ == '__main__':
 	main()
